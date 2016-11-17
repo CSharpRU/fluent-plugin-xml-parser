cb8d38774ce69e1a1a8d54bb68e8fe66c635b2fc
Yaroslav Lukyanov <c_sharp@mail.ru>
2016-11-17 16:36:50 +0700

    Disable convert by default

--- lib/fluent/plugin/filter_xml_simple.rb
+++ lib/fluent/plugin/filter_xml_simple.rb
@@ -6,7 +6,7 @@ module Fluent
 
     config_param :fields, :string
 
-    config_param :try_convert_times, :bool, :default => true # try to convert values in hash to times
+    config_param :try_convert_times, :bool, :default => false # try to convert values in hash to timestamps
     config_param :time_format, :string, :default => nil
     config_param :field_name_postfix, :string, :default => 'hash' # if set will create hash in new field with postfix (xml => xml_hash)
 
@@ -55,14 +55,28 @@ module Fluent
 
     private
 
-    def convert_times(hash)
-      hash.map { |key, value|
-        new_value = value.class == Hash ? convert_times(value) : try_to_convert(value) { |x|
+    def convert_to_time(value)
+      if value.class == Hash
+        convert_times(value)
+      elsif value.class == Array
+        convert_times_array(value)
+      else
+        try_to_convert(value) { |x|
           (self.time_format ? Time.strptime(x, self.time_format) : Time.parse(x)).to_i
         }
+      end
+    end
 
-        [key, new_value]
+    def convert_times(hash)
+      hash.map { |key, value|
+        [key, convert_to_time(value)]
       }.to_h
+    end
+
+    def convert_times_array(array)
+      array.map { |value|
+        convert_to_time(value)
+      }
     end
 
     def try_to_convert(value, &block)
